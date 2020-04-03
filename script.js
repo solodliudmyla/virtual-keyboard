8a271f9b6eff4dfcfcbedc4153fc9593d42b8056
Solod Liudmyla <solodludmila@gmail.com>
2020-04-04 01:45:42 +0300

    feat: add key-code array and change id to key-code

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ window.onload = function onLoad() {
 
     const getMatrix = (language) => {
       const matrix = {
+        code:[
+          ['Escape', 'F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','Delete'],
+          ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+          ['Tab','KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
+          ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+          ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight'],
+          ['ControlLeft','MetaLeft','AltLeft','Space','AltRight','ControlRight','ArrowLeft','ArrowUp','ArrowDown','ArrowRight']
+        ],
         en: [
           ['Esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Delete'],
           ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
@@ -23,6 +31,7 @@ window.onload = function onLoad() {
       return matrix[language];
     };
     let arrayButtons = getMatrix('en');
+    let arrayKeyCode = getMatrix('code');
 
     let createVewKeyboard = () => {
       const keyboard = document.createElement('div');
@@ -39,7 +48,7 @@ window.onload = function onLoad() {
           keyboardRow.append(tagLi);
           let tagA = document.createElement('a');
           tagA.setAttribute('class', 'key');
-          tagA.setAttribute('id', `id-${i}-${j}`);
+          tagA.setAttribute('id',`${arrayKeyCode[i][j]}`);
           if (i === 0) {tagA.setAttribute('class', 'key fn')}
           tagLi.append(tagA);
           let tagSpan = document.createElement('span');
